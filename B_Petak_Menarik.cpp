27b1c75ab574fa888d72b0f000e1509cec3744e7
Lin1er <m.ulinasidiki@gmail.com>
2023-11-15 12:46:49 +0700

    new lagi

--- B_Petak_Menarik.cpp
+++ B_Petak_Menarik.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 int main(){
     int n, m, k;
-    cin >n>>m>>k;
+    cin >>n>>m>>k;
     vector<vector<int>> papanGame(n, vector<int>(m));
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++) cin>>papanGame[i][j];
     }
-
+    for(int j=0; j<m;j++){
+        for(int i =0; i<n; i++){
+            if((i==0 ? 1 : papanGame[i-1][j])*(i==n-1 ? 1 : papanGame[i+1][j])*(j==0 ? 1 : papanGame[i][j-1])*(j==m-1 ? 1 : papanGame[i][j+1])==k){
+                cout<<(i+1)<<" "<<(j+1)<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"0 0";
+    return 0;
 }
---
+++ C_Wartel.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Contact{
+    string nama;
+    int nomor;
+}; 
+
+int main(){
+    int n, cari;
+    cin>>n>>cari;
+    string caris[cari];
+    Contact kontak[n];
+    for(int i = 0; i<n; i++) cin>>kontak[i].nama>>kontak[i].nomor;
+    for (int i = 0; i < cari; i++) cin>>caris[i];
+    for(int i=0; i<cari; i++){
+        for (int j = 0; j < n; j++){
+            size_t ketemu = kontak[j].nama.find(caris[i]);
+            if(ketemu!=string::npos){ 
+                cout<<kontak[j].nomor<<endl;
+                break;
+            }
+            if(j==n-1&&ketemu==string::npos) cout<<"NIHIL"<<endl;
+        }
+    }
+    
+}
