28f99aebf5fb8380a6fb243223bc65971047e0fc
Lin1er <m.ulinasidiki@gmail.com>
2023-11-16 16:31:43 +0700

    new

--- A_Refleksi_Matriks.cpp
+++ A_Refleksi_Matriks.cpp
@@ -17,11 +17,86 @@ int main(){
             cin>>mx2[i][j];
         }
     }
+    //identik
     nilaiMatriks=1;
     for (int i = 0; i < n; i++){
-        for (int i = 0; i < n; i++){
-            
+        for (int j = 0; j < n; j++){
+            if(mx1[i][j]!=mx2[i][j]){
+                nilaiMatriks=0;
+                break;
+            }
         }
+        if(nilaiMatriks==0) break;
     }
-    
+    if(nilaiMatriks==1){ 
+        cout<<"identik";
+        return 0;
+    }
+
+    //vertikal
+    nilaiMatriks=2;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(mx1[i][j]!=mx2[i][n-j-1]){
+                nilaiMatriks=0;
+                break;
+            }
+        }
+        if(nilaiMatriks==0) break;
+    }
+    if(nilaiMatriks==2){
+        cout<<"vertikal";
+        return 0;
+    }
+
+    //horizontal
+    nilaiMatriks=3;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(mx1[i][j]!=mx2[n-i-1][j]){
+                nilaiMatriks=0;
+                break;
+            }
+        }
+        if(nilaiMatriks==0) break;
+    }
+    if(nilaiMatriks==3){
+        cout<<"horisontal";
+        return 0;
+    }
+
+    //diagonal kanan bawah
+    nilaiMatriks = 4;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mx1[i][j] != mx2[j][i]) {
+                nilaiMatriks = 0;
+                break;
+            }
+        }
+        if (nilaiMatriks == 0) break;
+    }
+    if (nilaiMatriks == 4) {
+        cout << "diagonal kanan bawah";
+        return 0;
+    }
+
+    //diagonal kiri bawah
+    nilaiMatriks = 5;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mx1[i][j] != mx2[n-j-1][n-i-1]) {
+                nilaiMatriks = 0;
+                break;
+            }
+        }
+        if (nilaiMatriks == 0) break;
+    }
+    if (nilaiMatriks == 5) {
+        cout << "diagonal kiri bawah";
+        return 0;
+    }
+
+    cout<<"tidak identik";
+    return 0;
 }
